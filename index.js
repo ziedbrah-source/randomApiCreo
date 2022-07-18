a6b87a8d255a15f0e40ec67b71ee7eb71ea58471
Mohamed Zied Brahmi <ziedbrahmi2@gmail.com>
2022-07-19 00:08:23 +0100

    Added a Procfile

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/wallets/:wallet', (req, res) => {
+  const randomRes = Math.floor(Math.random() * 10);
+  const wallet = req.params.wallet;
+  res.status(200).json({ coins: randomRes });
+});
+
+app.get('/', (req, res) => {
+  res.send('WELCOME TO CREOMETRY COMMUNITY CLUSTER');
+});
+
+app.listen(port, () => {
+  console.log('APP LISTENING on port : ' + port);
+});
